732a584860e8edcc448aa4090ed35fe515f2da32
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2019-02-27 01:39:29 -0500

    inclusion of addemployee form

--- server.js
+++ server.js
@@ -5,14 +5,35 @@ const exphbs = require('express-handlebars');
 const HTTP_PORT = process.env.PORT || 8080;
 app.engine('.hbs',exphbs({
     extname: '.hbs',
-    defaultLayout: 'main'
+    defaultLayout: 'main',
+    helpers: {
+        navLink: (url,options)=>{
+            return '<a ' +((url == app.locals.activeRoute) ? 'class="nav-item nav-link active"' : 'class= "nav-item nav-link"')
+                    +' href="' + url + '">' + options.fn(this) + ((url == app.locals.activeRoute) ? '<span class="sr-only">(current)</span>' : '') 
+                    +'</a>';
+        }
+    }
 }));
 app.set('view engine','.hbs');
 
 app.use(express.static('public'));
+app.use((request,response,next)=>{
+    let route = request.baseUrl +request.path;
+    app.locals.activeRoute = (route == '/') ? '/' : route.replace(/\/$/,"");
+    next(); 
+});
+
 
 app.get("/",(request,response)=>{
     response.render('index');
+});
+
+app.get("/addemployee",(request,response)=>{
+    response.render('addemployee');
+});
+
+app.get("/images",(request,response)=>{
+    response.render('images');
 });
 
 app.listen(HTTP_PORT,()=>{
